b13dc87c754c8b0f7e0853da09899e5f9fc9ec1c
Roger <rogerjaffe@gmail.com>
2014-11-13 11:43:17 -0800

    Duh!

--- src/com/mrjaffesclass/apcs/mvc/template/Constants.java
+++ src/com/mrjaffesclass/apcs/mvc/template/Constants.java
@@ -16,5 +16,5 @@ public class Constants {
   public static final int DOWN = -1;
   
   public static final int FIELD_1_INCREMENT = 1;
-  public static final int FIELD_2_INCREMENT = 2;
+  public static final int FIELD_2_INCREMENT = 1;
 }
