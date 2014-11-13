17d9278e9ac3a107d2814a75058afc64f5b3bc20
Roger <rogerjaffe@gmail.com>
2014-11-13 11:40:11 -0800

    Add field increment constants

--- src/com/mrjaffesclass/apcs/mvc/template/Constants.java
+++ src/com/mrjaffesclass/apcs/mvc/template/Constants.java
@@ -14,6 +14,7 @@ public class Constants {
     // Constants
   public static final int UP = 1;
   public static final int DOWN = -1;
-
-
+  
+  public static final int FIELD_1_INCREMENT = 1;
+  public static final int FIELD_2_INCREMENT = 2;
 }
